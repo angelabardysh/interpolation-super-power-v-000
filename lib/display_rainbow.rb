ea5bca0680abaa0dd2ba2cc622d765ca97867e10
Angela Bardysh <angelabardysh@gmail.com>
2019-05-09 01:48:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,4 @@ def display_rainbow(array_of_colors)
   puts "R: #{display_rainbow[0]} , O: #{display_rainbow[1]}, Y: #{display_rainbow[3]}, G: #{display_rainbow[4]}, B: #{display_rainbow[5]}, I: #{display_rainbow[6]}, V: #{display_rainbow[7]}"
 end
 
-display_rainbow
+display_rainbow(array_of_colors)
