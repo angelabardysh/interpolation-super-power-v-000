c5667510d1cb9943bbe014a5aa392a75a3a12ac5
Angela Bardysh <angelabardysh@gmail.com>
2019-05-09 01:28:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,4 @@ def display_rainbow("red", "orange", "yellow", "green", "blue", "indigo", "viole
   puts "R: #{display_rainbow[0]} , O: #{display_rainbow[1]}, Y: #{display_rainbow[3]}, G: #{display_rainbow[4]}, B: #{display_rainbow[5]}, I: #{display_rainbow[6]}, V: #{display_rainbow[7]}"
 end
 
-display_rainbow
+display_rainbow("red")
