ce19a05d1d5746b586214fb9b86f9c568e94f34f
Angela Bardysh <angelabardysh@gmail.com>
2019-05-09 01:58:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 
 
 def display_rainbow(colors)
-  puts "R: #{display_rainbow[0]} , O: #{display_rainbow[1]}, Y: #{display_rainbow[3]}, G: #{display_rainbow[4]}, B: #{display_rainbow[5]}, I: #{display_rainbow[6]}, V: #{display_rainbow[7]}"
+  puts "R: #{colors[0]} , O: #{colors[1]}, Y: #{colors[3]}, G: #{colors[4]}, B: #{colors[5]}, I: #{colors[6]}, V: #colors[7]}"
 end
